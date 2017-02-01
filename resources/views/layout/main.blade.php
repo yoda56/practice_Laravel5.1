e4b5fb5d4ccf8e72626564fee513be79cfe0f82d
yoda56 <info@halfmoonra.sakura.ne.jp>
2017-02-02 00:09:49 +0900

    認証回り実装

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>テストサイト</title>
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+@yield('content')
